7e3ec6258274069da8d0dd21896329a0d913085b
hyunmo <jrsky723@gmail.com>
2024-01-04 19:41:53 +0900

    # 9.5 CupertinoSearchTextField
    - CupertinoSearchTextField: ios에서 사용하는 검색창

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,11 @@ class TikTokApp extends StatelessWidget {
         primaryColor: const Color(
           0xFFE9435A,
         ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(
+            0xFFE9435A,
+          ),
+        ),
         appBarTheme: const AppBarTheme(
           surfaceTintColor: Colors.white,
           foregroundColor: Colors.black,
