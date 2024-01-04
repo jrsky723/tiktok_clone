a4c555577576a887ad39aaa3df3c606ec7493d7f
hyunmo <jrsky723@gmail.com>
2024-01-04 18:08:06 +0900

    # 9.2 TabBar
    - Bottom은 preferredSizeWidget을 사용한다.
      - 특정한 크기를 가지지만, child의 크기제한이 없다.

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Discover"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text("Discover"),
+          bottom: TabBar(
+            tabAlignment: TabAlignment.start,
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            isScrollable: true,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            indicatorColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            labelColor: Colors.black,
+            tabs: [
+              for (final tab in tabs) Tab(text: tab),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (final tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
