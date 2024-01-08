0456e9847708a76adb953fa7dbe4ad63feeaa05e
hyunmo <jrsky723@gmail.com>
2024-01-08 10:35:43 +0900

    # 10.2 Dismissible
    - Dismissible: 리스트뷰의 아이템을 스와이프하여 삭제할 수 있는 위젯
    - 두 가지의 background를 설정할 수 있다.
      - secondaryBackground: 왼쪽으로 스와이프 했을 때
      - background: 오른쪽으로 스와이프 했을 때

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
+import 'package:tiktok_clone/features/inbox/activity_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -45,7 +44,7 @@ class TikTokApp extends StatelessWidget {
           backgroundColor: Colors.white,
         ),
       ),
-      home: const MainNavigationScreen(),
+      home: const ActivityScreen(),
     );
   }
 }
