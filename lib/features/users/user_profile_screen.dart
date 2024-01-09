6fe56867baccd7f4d6d004beec3fc88170a1ee4e
hanu607 <jrsky723@gmail.com>
2024-01-09 21:18:19 +0900

    # 12.2 SliverAppBar
    - SliverFixedExtentList: 스크롤을 할 때, 높이가 고정된 리스트를 만들 수 있다.

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -13,9 +13,10 @@ class _UserProfileScreenState extends State {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
+          snap: true,
           floating: true,
-          stretch: true,
           pinned: true,
+          stretch: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
           expandedHeight: 200,
@@ -25,6 +26,7 @@ class _UserProfileScreenState extends State {
             stretchModes: const [
               StretchMode.zoomBackground,
               StretchMode.blurBackground,
+              StretchMode.fadeTitle,
             ],
             background: Image.asset(
               "assets/images/placeholder.jpg",
@@ -35,7 +37,14 @@ class _UserProfileScreenState extends State {
         ),
         SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
-            (context, index) => Container(),
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
           ),
           itemExtent: 100,
         ),
