60631a3b69e164873fcf6bf8e08785ecf3175701
hanu607 <jrsky723@gmail.com>
2024-01-10 14:26:25 +0900

    # 13.2 showDateRangePicker
    - Material design의 날짜 범위 선택 다이얼로그를 띄움

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -11,8 +11,44 @@ class SettingsScreen extends StatelessWidget {
         title: const Text("Settings"),
       ),
       body: ListView(
-        children: const [
-          AboutListTile(),
+        children: [
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1990),
+                lastDate: DateTime(2030),
+              );
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1990),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: Theme.of(context).copyWith(
+                      colorScheme: const ColorScheme.light(
+                        primary: Colors.amberAccent,
+                        onPrimary: Colors.redAccent,
+                        onSurface: Colors.blueAccent,
+                      ),
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
