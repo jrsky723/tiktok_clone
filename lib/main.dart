3063a07afdeb2834957506ee5040867d0385f648
hyunmo <jrsky723@gmail.com>
2024-01-15 14:59:34 +0900

    # 16.6 Numbers l10n

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 import 'package:tiktok_clone/generated/l10n.dart';
 
 void main() async {
@@ -121,7 +122,7 @@ class TikTokApp extends StatelessWidget {
           iconColor: Colors.white,
         ),
       ),
-      home: const SignUpScreen(),
+      home: const MainNavigationScreen(),
     );
   }
 }
