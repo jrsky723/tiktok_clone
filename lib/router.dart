a441e6688eda08260aedc139b69721daa36b2fdb
hanu607 <jrsky723@gmail.com>
2024-01-16 15:17:30 +0900

    # 18.5 Conclusions
    - GoRouter는 React의 react-router와 유사하다.

--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/users/user_profile_screen.dart';
