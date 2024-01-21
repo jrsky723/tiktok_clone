ceda56141a8e2b39e8a831342d56b4e5b51e03ed
hanu607 <jrsky723@gmail.com>
2024-01-21 21:03:59 +0900

    # 20.8 Recap
    - InheritedWidget은 위젯 트리를 거치지 않고, 데이터를 전달할 수 있다.
    - 이 접근 방식의 문제는 too verbose(너무 많은 코드)이다.
    - 이를 해결하기 위해, ChangeNotifierProvider를 사용한다.

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:marquee/marquee.dart';
-import 'package:tiktok_clone/common/widgets/video_configuration/video_config.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/videos/widgets/video_button.dart';
