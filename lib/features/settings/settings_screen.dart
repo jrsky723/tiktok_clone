02cd857aeb0da4bc1dfd48ab5dce03cc2e10286e
hyunmo <jrsky723@gmail.com>
2024-01-22 17:14:01 +0900

    # 22.2 ConsumerWidget
    - build method: Notifier가 노출할 데이터를 제공하는 방법
    - View들을 playbackConfig에 연결
    - ConsumerWidget
      - build method가 Widget Referenece를 받는다.
    - vsc의 Flutter Riverpod Snippets를 사용하면, 쉽게 작성할 수 있다.

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:provider/provider.dart';
+
 import 'package:tiktok_clone/common/widgets/dark_mode_config/dark_mode_config.dart';
 import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/features/videos/view_models/playback_config_vm.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
   @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  bool _notifications = false;
-
-  void _onNotificationsChanged(bool? newValue) {
-    if (newValue == null) return;
-    setState(() {
-      _notifications = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final width = MediaQuery.of(context).size.width;
     return Localizations.override(
       context: context,
@@ -53,28 +40,34 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   ),
                 ),
                 SwitchListTile.adaptive(
-                  value: false,
-                  onChanged: (value) => {},
+                  value: ref.watch(playbackConfigProvider).muted,
+                  onChanged: (value) => {
+                    ref.read(playbackConfigProvider.notifier).setMuted(value),
+                  },
                   title: const Text("Mute Video"),
                   subtitle: const Text("Video will be muted by default."),
                 ),
                 SwitchListTile.adaptive(
-                  value: false,
-                  onChanged: (value) => {},
+                  value: ref.watch(playbackConfigProvider).autoplay,
+                  onChanged: (value) => {
+                    ref
+                        .read(playbackConfigProvider.notifier)
+                        .setAutoplay(value),
+                  },
                   title: const Text("Autoplay"),
                   subtitle:
                       const Text("Video will start playing automatically."),
                 ),
                 SwitchListTile.adaptive(
-                  value: _notifications,
-                  onChanged: _onNotificationsChanged,
+                  value: false,
+                  onChanged: (value) {},
                   title: const Text("Enable notifications"),
                   subtitle: const Text("They will be cute."),
                 ),
                 CheckboxListTile(
                   activeColor: Colors.black,
-                  value: _notifications,
-                  onChanged: _onNotificationsChanged,
+                  value: false,
+                  onChanged: (value) {},
                   title: const Text("Marketing emails"),
                   subtitle: const Text("We won't spam you."),
                 ),
@@ -89,7 +82,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     if (kDebugMode) {
                       print(date);
                     }
-                    if (!mounted) return;
+
                     final time = await showTimePicker(
                       context: context,
                       initialTime: TimeOfDay.now(),
@@ -97,7 +90,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     if (kDebugMode) {
                       print(time);
                     }
-                    if (!mounted) return;
+
                     final booking = await showDateRangePicker(
                       context: context,
                       firstDate: DateTime(1980),
