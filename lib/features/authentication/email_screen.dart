1d77215a05556e4368c8d724bfd3d1658238bab6
hanu607 <jrsky723@gmail.com>
2024-01-27 12:32:13 +0900

    # 24.0 createUserWithEmailAndPassword
    - 이메일과 비밀번호를 사용해서 계정을 만든다.
    - SignUpViewModel은 아무것도 expose하지 않는다.
      - 로딩화면을 보여주고 계정 생성을 시도한다. (로딩여부말고없음)
    - StateProvider: 바깥쪽에서 수정할 수 있는 value를 expose하게한다.
    - 인증과정에서는 email과 password만 필요 (생일 제외)
    - AsyncValue.guard: 로딩중일 때, 로딩화면을 보여준다.
    - signUp은 별도의 provider, form에서 데이터를 읽고, 그 다음 repositor를 호출. repository는 firebase를 호출한다.

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/password_screen.dart';
+import 'package:tiktok_clone/features/authentication/view_models/signup_view_model.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
 class EmailScreenArgs {
@@ -10,16 +12,16 @@ class EmailScreenArgs {
   EmailScreenArgs({required this.username});
 }
 
-class EmailScreen extends StatefulWidget {
+class EmailScreen extends ConsumerStatefulWidget {
   final String username;
 
   const EmailScreen({super.key, required this.username});
 
   @override
-  State<EmailScreen> createState() => _EmailScreenState();
+  ConsumerState<EmailScreen> createState() => _EmailScreenState();
 }
 
-class _EmailScreenState extends State<EmailScreen> {
+class _EmailScreenState extends ConsumerState<EmailScreen> {
   final TextEditingController _emailController = TextEditingController();
 
   String _email = "";
@@ -56,6 +58,7 @@ class _EmailScreenState extends State<EmailScreen> {
 
   void _onNextTap() {
     if (_email.isEmpty || _isEmailValid() != null) return;
+    ref.read(signUpForm.notifier).state = {"email": _email};
     Navigator.push(
       context,
       MaterialPageRoute(
