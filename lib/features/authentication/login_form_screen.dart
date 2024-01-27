7193c1433f2117d073be60a655df65b6f8822e46
hanu607 <jrsky723@gmail.com>
2024-01-27 16:36:40 +0900

    # 24.2 signInWithEmailAndPassword
    - snackbar를 사용해서, 에러를 보여준다.
      - snackbar는 context가 필요하다.
    - ref.read(loginProvider.notifier).~: notifier는 provider의 상태 감지를 제외하고, 함수를 사용할 수 있다.
    - NotifierProvider의 두 번째 제네릭 타입은, 상태를 나타내는 모델을 사용, 데이터를 포함해야한다.

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/view_models/login_view_model.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
-import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
-class LoginFormScreen extends StatefulWidget {
+class LoginFormScreen extends ConsumerStatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
-  State<LoginFormScreen> createState() => _LoginFormScreenState();
+  ConsumerState<LoginFormScreen> createState() => _LoginFormScreenState();
 }
 
-class _LoginFormScreenState extends State<LoginFormScreen> {
+class _LoginFormScreenState extends ConsumerState<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  final Map<String, String> _formData = {
-    "email": "",
-    "password": "",
-  };
+  final Map<String, String> formData = {};
 
   void _onLoginTap() {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        context.goNamed(
-          InterestsScreen.routeName,
-        );
+        ref.read(loginProvider.notifier).login(
+              formData["email"]!,
+              formData["password"]!,
+              context,
+            );
+        // ref.read(loginProvider.notifier).login(formData["email"], formData["password"], context);
+        // log user in
+        // context.goNamed(
+        //   InterestsScreen.routeName,
+        // );
       }
     }
   }
@@ -73,7 +77,7 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                       return null;
                     },
                     onSaved: (newValue) {
-                      if (newValue != null) _formData["email"] = newValue;
+                      if (newValue != null) formData["email"] = newValue;
                     }),
                 Gaps.v16,
                 TextFormField(
@@ -98,10 +102,14 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                       return null;
                     },
                     onSaved: (newValue) {
-                      if (newValue != null) _formData["password"] = newValue;
+                      if (newValue != null) formData["password"] = newValue;
                     }),
                 Gaps.v28,
-                FormButton(disabled: false, text: "Log in", onTap: _onLoginTap),
+                FormButton(
+                  disabled: ref.watch(loginProvider).isLoading,
+                  text: "Log in",
+                  onTap: _onLoginTap,
+                ),
               ],
             ),
           ),
