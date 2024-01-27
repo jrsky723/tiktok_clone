cb125af36cbeda6af88797ca85e8dd23c467af26
hanu607 <jrsky723@gmail.com>
2024-01-27 14:41:45 +0900

    # 24.1 signOut
    - stream을 사용해서, 로그인 상태를 확인한다.
      - stream은 계속해서 값을 받아온다.
    - 백엔드와 UI의 연결
    - authstate가 변화되면 provider가 재생성된다.
    - signOut은 간단한 기능이므로, repo와 view가 직접 연결
    - 즉시 redirect 하게 된다.

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -3,9 +3,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:tiktok_clone/common/widgets/dark_mode_config/dark_mode_config.dart';
 import 'package:tiktok_clone/constants/breakpoints.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/videos/view_models/playback_config_vm.dart';
 
 class SettingsScreen extends ConsumerWidget {
@@ -127,7 +130,10 @@ class SettingsScreen extends ConsumerWidget {
                             child: const Text("No"),
                           ),
                           CupertinoDialogAction(
-                            onPressed: () => Navigator.of(context).pop(),
+                            onPressed: () {
+                              ref.read(authRepo).signOut();
+                              context.go(SignUpScreen.routeURL);
+                            },
                             isDestructiveAction: true,
                             child: const Text("Yes"),
                           ),
