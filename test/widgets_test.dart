626dfd0eb97332f7f731ee4861f2e4266f2c29ee
hanu607 <jrsky723@gmail.com>
2024-02-08 23:21:14 +0900

    # 30.5 Widget Testing part Two
    - FormButton 내부의 isDarkMode를 테스트하기 위해선, MediaQuery를 감싸야한다.

--- test/widgets_test.dart
+++ test/widgets_test.dart
@@ -3,28 +3,131 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tiktok_clone/features/authentication/views/widgets/form_button.dart';
 
 void main() {
-  group("Form Button Tests", () {
-    testWidgets("Enables State", (WidgetTester tester) async {
-      await tester.pumpWidget(
-        Directionality(
-          textDirection: TextDirection.ltr,
-          child: FormButton(
-            disabled: false,
-            text: "text",
-            onTap: () {},
+  group(
+    "Form Button Tests",
+    () {
+      testWidgets("Enables State", (WidgetTester tester) async {
+        await tester.pumpWidget(
+          Theme(
+            data: ThemeData(
+              primaryColor: Colors.red,
+            ),
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: FormButton(
+                disabled: false,
+                text: "text",
+                onTap: () {},
+              ),
+            ),
           ),
-        ),
+        );
+
+        expect(find.text("text"), findsOneWidget);
+
+        expect(
+          tester
+              .firstWidget<AnimatedDefaultTextStyle>(
+                  find.byType(AnimatedDefaultTextStyle))
+              .style
+              .color,
+          Colors.white,
+        );
+
+        expect(
+          (tester
+                  .firstWidget<AnimatedContainer>(
+                      find.byType(AnimatedContainer))
+                  .decoration as BoxDecoration)
+              .color,
+          Colors.red,
+        );
+      });
+
+      testWidgets(
+        "Disabled State",
+        (WidgetTester tester) async {
+          await tester.pumpWidget(
+            MediaQuery(
+              data: const MediaQueryData(),
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: FormButton(
+                  disabled: true,
+                  text: "text",
+                  onTap: () {},
+                ),
+              ),
+            ),
+          );
+          expect(find.text("text"), findsOneWidget);
+          expect(
+            tester
+                .firstWidget<AnimatedDefaultTextStyle>(
+                    find.byType(AnimatedDefaultTextStyle))
+                .style
+                .color,
+            Colors.grey.shade400,
+          );
+        },
       );
 
-      expect(find.text("text"), findsOneWidget);
-      expect(
-        tester
-            .firstWidget<AnimatedDefaultTextStyle>(
-                find.byType(AnimatedDefaultTextStyle))
-            .style
-            .color,
-        Colors.white,
+      testWidgets(
+        "Disabled State Dark Mode",
+        (WidgetTester tester) async {
+          await tester.pumpWidget(
+            MediaQuery(
+              data: const MediaQueryData(
+                platformBrightness: Brightness.dark,
+              ),
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: FormButton(
+                  disabled: true,
+                  text: "text",
+                  onTap: () {},
+                ),
+              ),
+            ),
+          );
+          expect(
+            (tester
+                    .firstWidget<AnimatedContainer>(
+                        find.byType(AnimatedContainer))
+                    .decoration as BoxDecoration)
+                .color,
+            Colors.grey.shade800,
+          );
+        },
       );
-    });
-  });
+      testWidgets(
+        "Disabled State Light Mode",
+        (WidgetTester tester) async {
+          await tester.pumpWidget(
+            MediaQuery(
+              data: const MediaQueryData(
+                platformBrightness: Brightness.light,
+              ),
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: FormButton(
+                  disabled: true,
+                  text: "text",
+                  onTap: () {},
+                ),
+              ),
+            ),
+          );
+          expect(
+            (tester
+                    .firstWidget<AnimatedContainer>(
+                        find.byType(AnimatedContainer))
+                    .decoration as BoxDecoration)
+                .color,
+            Colors.grey.shade300,
+          );
+        },
+      );
+    },
+  );
 }
