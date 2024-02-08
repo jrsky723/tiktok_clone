9126b4455887f2bda72d141ccc14d9b7df42c27d
hanu607 <jrsky723@gmail.com>
2024-02-08 18:04:22 +0900

    # 29.4 ShellRoute
    - 알림을 클릭하여 원하는 페이지로 이동하고 싶다.
    - provider에 context를 넘겨주는 방법
    - notificationProvider에 context를 넘길때, router가 생기기 이전에 context를 넘기면, goRouter의 go함수를 사용할 수 없다.
    - shell route는 route를 감싸서, provider에 context를 넘길 수 있다.

--- lib/router.dart
+++ lib/router.dart
@@ -8,6 +8,7 @@ import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/inbox/views/activity_screen.dart';
 import 'package:tiktok_clone/features/inbox/views/chat_detail_screen.dart';
 import 'package:tiktok_clone/features/inbox/views/chats_screen.dart';
+import 'package:tiktok_clone/features/notifications/notifications_provider.dart';
 import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 import 'package:tiktok_clone/features/videos/views/video_recording_screen.dart';
 
@@ -28,72 +29,80 @@ final routerProvider = Provider(
         return null;
       },
       routes: [
-        GoRoute(
-          name: SignUpScreen.routeName,
-          path: SignUpScreen.routeURL,
-          builder: (context, state) => const SignUpScreen(),
-        ),
-        GoRoute(
-          name: LoginScreen.routeName,
-          path: LoginScreen.routeURL,
-          builder: (context, state) => const LoginScreen(),
-        ),
-        GoRoute(
-          name: InterestsScreen.routeName,
-          path: InterestsScreen.routeURL,
-          builder: (context, state) => const InterestsScreen(),
-        ),
-        GoRoute(
-          path: "/:tab(home|discover|inbox|profile)",
-          name: MainNavigationScreen.routeName,
-          builder: (context, state) {
-            final String tab = state.params["tab"]!;
-            return MainNavigationScreen(tab: tab);
+        ShellRoute(
+          builder: (context, state, child) {
+            ref.read(notificationsProvider(context));
+            return child;
           },
-        ),
-        GoRoute(
-          path: ActivityScreen.routeURL,
-          name: ActivityScreen.routeName,
-          builder: (context, state) => const ActivityScreen(),
-        ),
-        GoRoute(
-          path: ChatsScreen.routeURL,
-          name: ChatsScreen.routeName,
-          builder: (context, state) => const ChatsScreen(),
           routes: [
             GoRoute(
-              path: ChatDetailScreen.routeURL,
-              name: ChatDetailScreen.routeName,
+              name: SignUpScreen.routeName,
+              path: SignUpScreen.routeURL,
+              builder: (context, state) => const SignUpScreen(),
+            ),
+            GoRoute(
+              name: LoginScreen.routeName,
+              path: LoginScreen.routeURL,
+              builder: (context, state) => const LoginScreen(),
+            ),
+            GoRoute(
+              name: InterestsScreen.routeName,
+              path: InterestsScreen.routeURL,
+              builder: (context, state) => const InterestsScreen(),
+            ),
+            GoRoute(
+              path: "/:tab(home|discover|inbox|profile)",
+              name: MainNavigationScreen.routeName,
               builder: (context, state) {
-                final chatRoomId = state.params["chatRoomId"]!;
-                final args = state.extra as ChatDetailScreenArgs;
-                return ChatDetailScreen(
-                  chatRoomId: chatRoomId,
-                  userId: args.userId,
-                  userName: args.userName,
-                  hasAvatar: args.hasAvatar,
-                );
+                final String tab = state.params["tab"]!;
+                return MainNavigationScreen(tab: tab);
               },
-            )
+            ),
+            GoRoute(
+              path: ActivityScreen.routeURL,
+              name: ActivityScreen.routeName,
+              builder: (context, state) => const ActivityScreen(),
+            ),
+            GoRoute(
+              path: ChatsScreen.routeURL,
+              name: ChatsScreen.routeName,
+              builder: (context, state) => const ChatsScreen(),
+              routes: [
+                GoRoute(
+                  path: ChatDetailScreen.routeURL,
+                  name: ChatDetailScreen.routeName,
+                  builder: (context, state) {
+                    final chatRoomId = state.params["chatRoomId"]!;
+                    final args = state.extra as ChatDetailScreenArgs;
+                    return ChatDetailScreen(
+                      chatRoomId: chatRoomId,
+                      userId: args.userId,
+                      userName: args.userName,
+                      hasAvatar: args.hasAvatar,
+                    );
+                  },
+                )
+              ],
+            ),
+            GoRoute(
+              path: VideoRecordingScreen.routeURL,
+              name: VideoRecordingScreen.routeName,
+              pageBuilder: (context, state) => CustomTransitionPage(
+                transitionDuration: const Duration(milliseconds: 200),
+                child: const VideoRecordingScreen(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  final position =
+                      Tween(begin: const Offset(0, 1), end: Offset.zero)
+                          .animate(animation);
+                  return SlideTransition(
+                    position: position,
+                    child: child,
+                  );
+                },
+              ),
+            ),
           ],
-        ),
-        GoRoute(
-          path: VideoRecordingScreen.routeURL,
-          name: VideoRecordingScreen.routeName,
-          pageBuilder: (context, state) => CustomTransitionPage(
-            transitionDuration: const Duration(milliseconds: 200),
-            child: const VideoRecordingScreen(),
-            transitionsBuilder:
-                (context, animation, secondaryAnimation, child) {
-              final position =
-                  Tween(begin: const Offset(0, 1), end: Offset.zero)
-                      .animate(animation);
-              return SlideTransition(
-                position: position,
-                child: child,
-              );
-            },
-          ),
         ),
       ],
     );
