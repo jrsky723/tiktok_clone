14e51c04139cf51e0d0eff94d0e3c69f4d5877ad
hanu607 <jrsky723@gmail.com>
2024-02-08 17:41:22 +0900

    # 29.3 Background Notifications
    - 유저가 알림을 언제 열었는지 아는 방법
    - scenario1: 앱이 background일때, user가 알림을 연다.
    - scenario2: 앱이 terminated일때, user가 알림을 연다.
      - 앱이 notification에 의해 열렸는지 확인할 수 있다.
    - 원하는 유저에게 타겟해서 알림을 보낼 수 있다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,12 +13,20 @@ import 'package:tiktok_clone/firebase_options.dart';
 import 'package:tiktok_clone/generated/l10n.dart';
 import 'package:tiktok_clone/router.dart';
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  print('Handling a background message ${message.messageId}');
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   // await SystemChrome.setPreferredOrientations(
   //   [
